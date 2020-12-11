c79f0337607f0ef10fc4f6d0f09e3399a0d9370f
Siarhei Charniak <siarhei_charniak@epam.com>
2020-12-11 14:34:20 +0300

    EDGDEMATIC-5 - Project Setup: edge-dematic

---
+++ src/main/java/org/folio/ed/EdgeDematicApplication.java
@@ -0,0 +1,11 @@
+package org.folio.ed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdgeDematicApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(EdgeDematicApplication.class, args);
+  }
+}
---
+++ src/main/java/org/folio/ed/config/EdgeDematicSpringConfiguration.java
@@ -0,0 +1,7 @@
+package org.folio.ed.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class EdgeDematicSpringConfiguration {
+}
