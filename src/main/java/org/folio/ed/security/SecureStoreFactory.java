f16c92e98d16ed1ac801c61e005c01547d4b9830
khandramai <Viachaslau_Khandramai@epam.com>
2021-03-02 01:12:02 +0300

    EDGEDEMATIC-9 # Disabling vault

--- src/main/java/org/folio/ed/security/SecureStoreFactory.java
+++ src/main/java/org/folio/ed/security/SecureStoreFactory.java
@@ -5,7 +5,6 @@ import java.util.Properties;
 import org.folio.edge.core.security.AwsParamStore;
 import org.folio.edge.core.security.EphemeralStore;
 import org.folio.edge.core.security.SecureStore;
-import org.folio.edge.core.security.VaultStore;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -18,24 +17,14 @@ public class SecureStoreFactory {
   public static SecureStore getSecureStore(String type, Properties props) {
     SecureStore ret;
 
-
-
-    if (type == null)
-      type = "";
-
-    switch (type) {
-    case VaultStore.TYPE:
-      ret = new VaultStore(props);
-      break;
-    case AwsParamStore.TYPE:
+    if (AwsParamStore.TYPE.equals(type)) {
       ret = new TenantAwareAWSParamStore(props);
-      break;
-    case EphemeralStore.TYPE:
-    default:
+    } else {
       ret = new EphemeralStore(props);
     }
 
-    log.info("type: {}, class: {}", type, ret.getClass().getName());
+    log.info("type: {}, class: {}", type, ret.getClass()
+      .getName());
     return ret;
   }
 
