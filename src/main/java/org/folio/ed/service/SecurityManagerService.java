46e14575cebf5ddf4414624c08ec0d141e2cd271
khandramai <Viachaslau_Khandramai@epam.com>
2021-03-05 18:07:29 +0300

    FOLIO-3014 # Env configuration improvements

--- src/main/java/org/folio/ed/service/SecurityManagerService.java
+++ src/main/java/org/folio/ed/service/SecurityManagerService.java
@@ -7,7 +7,6 @@ import static org.folio.edge.core.Constants.DEFAULT_SECURE_STORE_TYPE;
 import static org.folio.edge.core.Constants.PROP_SECURE_STORE_TYPE;
 import static org.folio.edge.core.Constants.X_OKAPI_TOKEN;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.net.URL;
@@ -135,25 +134,28 @@ public class SecurityManagerService {
     return connectionSystemParameters;
   }
 
-  private Properties getProperties(String secureStorePropFile) {
+  private static Properties getProperties(String secureStorePropFile) {
     Properties secureStoreProps = new Properties();
+
+    log.info("Attempt to load properties from: " + secureStorePropFile);
 
     if (secureStorePropFile != null) {
       URL url = null;
       try {
-        if (isURL.matcher(secureStorePropFile)
-          .matches()) {
+        if (isURL.matcher(secureStorePropFile).matches()) {
           url = new URL(secureStorePropFile);
         }
-        try (InputStream in = url == null ? new FileInputStream(new File(secureStorePropFile)) : url.openStream()) {
+
+        try (
+          InputStream in = url == null ? new FileInputStream(secureStorePropFile) : url.openStream()) {
           secureStoreProps.load(in);
           log.info("Successfully loaded properties from: " + secureStorePropFile);
         }
       } catch (Exception e) {
-        throw new AuthorizationException("Failed to load secure store properties");
+        log.warn("Failed to load secure store properties.", e);
       }
     } else {
-      log.warn("No secure store properties file specified. Using defaults");
+      log.warn("No secure store properties file specified.  Using defaults");
     }
     return secureStoreProps;
   }
