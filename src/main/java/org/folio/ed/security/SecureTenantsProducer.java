04b7d35494ceb4e0b0ebc327a8566e3b93256878
Aliaksei_Harbuz <Aliaksei_Harbuz@epam.com>
2021-04-23 14:46:03 +0300

    EDGDEMATIC-19 - secure tenants producer

---
+++ src/main/java/org/folio/ed/security/SecureTenantsProducer.java
@@ -0,0 +1,22 @@
+package org.folio.ed.security;
+
+import lombok.extern.log4j.Log4j2;
+import org.folio.edge.core.security.SecureStore;
+
+import java.util.Optional;
+import java.util.Properties;
+
+@Log4j2
+public class SecureTenantsProducer {
+
+  public static Optional<String> getTenants(Properties secureStoreProps, SecureStore secureStore, String stagingDirectorTenants) {
+    if (secureStore instanceof TenantAwareAWSParamStore) {
+      var stringOptional = ((TenantAwareAWSParamStore) secureStore).getTenants(stagingDirectorTenants);
+      if (stringOptional.isEmpty()) {
+        log.warn("Tenants list not found in AWS Param store. Please create variable, which contains comma separated list of tenants");
+      }
+      return stringOptional;
+    }
+    return Optional.of((String) secureStoreProps.get("tenants"));
+  }
+}
---
+++ src/test/java/org/folio/ed/security/SecureTenantsProducerTest.java
@@ -0,0 +1,30 @@
+package org.folio.ed.security;
+
+import org.folio.edge.core.security.EphemeralStore;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.Optional;
+import java.util.Properties;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+public class SecureTenantsProducerTest {
+
+  @Test
+  public void testGetTenants() {
+    var secureStoreProps = new Properties();
+    secureStoreProps.put("tenants", "test_tenant");
+
+    var stagingDirectorTenants = "stagingDirectorTenants";
+    var mockSecureStore = Mockito.mock(EphemeralStore.class);
+    Optional<String> tenants = SecureTenantsProducer.getTenants(secureStoreProps, mockSecureStore, stagingDirectorTenants);
+    assertEquals("test_tenant", tenants.get());
+
+    var mockAwsSecureStore  = Mockito.mock(TenantAwareAWSParamStore.class);
+    when(mockAwsSecureStore.getTenants(stagingDirectorTenants)).thenReturn(Optional.of("test_tenant_aws"));
+    tenants = SecureTenantsProducer.getTenants(secureStoreProps, mockAwsSecureStore, stagingDirectorTenants);
+    assertEquals("test_tenant_aws", tenants.get());
+  }
+}
